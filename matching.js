14c1faaa2fb7f9dc649b5753d8e8a398beecb022
Akash <deymrakash99@gmail.com>
2024-12-24 16:04:24 +0530

    add randomiser

--- matching.js
+++ matching.js
@@ -1,12 +1,17 @@
-const teams = [];
+const teams = [
+  { name: "team 1" },
+  { name: "team 2" },
+  { name: "team 3" },
+  { name: "team 4" },
+];
 
 const addTeams = () => {
   console.clear();
   const text1 = `Currently there are ${teams.length} team. press no to quit`;
   const text2 = "\n\nTeam Name : ";
   const teamName = prompt(text1 + text2);
-  
-  if (teamName === 'no') {
+
+  if (teamName === "no") {
     return;
   }
 
@@ -14,5 +19,11 @@ const addTeams = () => {
   addTeams();
 };
 
-addTeams();
+const randomiser = () => {
+  teams.sort((a, b) => Math.random() - 0.5);
+};
+
+// addTeams();
+// console.log(teams);
+randomiser();
 console.log(teams);
