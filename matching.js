6edeb9100be4317094f6587f207866230f383a09
Akash <deymrakash99@gmail.com>
2024-12-24 15:30:46 +0530

    initial commit

---
+++ matching.js
@@ -0,0 +1,9 @@
+const teams = [];
+
+const addTeams = () => {
+  const text1 = `Currently there are ${teams.length} do you want to add :`;
+  const text2 = '\n'
+  const teamName = prompt(text1);
+} 
+
+addTeams()
